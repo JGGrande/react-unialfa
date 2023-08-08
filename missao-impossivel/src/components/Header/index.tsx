96cacc743f568b4f6b6019b0031ca4f2015ba8a0
UniALFA <unialfa@MACMINI-02.local>
2023-08-07 22:22:21 -0300

    Criado Header e imagens

---
+++ missao-impossivel/src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+export function Header(){
+    return(
+        <>
+            <header className="header" >
+                <div className="containerLeftLogo">
+                    <img  />
+                </div>
+                <div className="contentRight">
+                    <ul>
+                        <li><strong>HOME</strong></li>
+                        <li><strong>SOBRE</strong></li>
+                        <li><strong>CONTATO</strong></li>
+                    </ul>
+                </div>
+            </header>
+        </>
+    )
+}
